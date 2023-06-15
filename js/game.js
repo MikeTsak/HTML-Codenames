72e0fbe2361cb7a78f91ed1af0f5b28aa6bbe7e1
Mike <32255755+MikeTsak@users.noreply.github.com>
2023-06-15 05:02:29 +0300

    images v1

--- js/game.js
+++ js/game.js
@@ -40,139 +40,246 @@ window.onclick = function(event) {
 
 let usedIndices = new Set();
 
-window.addEventListener('DOMContentLoaded', (event) => {
 
-    const giturl = chechPack();
-    fetch(giturl)
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById('grid-container');
-
-
-        const random = getRandomSequenceWithSeed(hash[1], data.length);
-
-        let redCount = 0;
-        let blueCount = 0;
-        let j = 25;
-
-        for(let i = 0; i < 25; i++){
-            const item = document.createElement('div');
-            item.className = 'grid-item';
-            item.dataset.color = colors[i];
-            // const random = Math.random();
-            // console.log(random);
+let chechPack = () => {
+    // Get the pack name from localStorage
+    var fragment = window.location.hash;
+    console.log(fragment);
+    fragment = fragment.split('-')[0];
+    if(fragment === '#1'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/defult.json']}
+    else if(fragment === '#2'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greek.json']}
+    else if(fragment === '#3'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/marvel.json']}
+    else if(fragment === '#4'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/starwars.json']}
+    else if(fragment === '#5'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greekhistory.json']}
+    else if(fragment === '#6'){
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/disney.json']}
+    else if(fragment === '#i6')
+    return [2,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/idisney.json']
     
-            // Generate a random index to pick an item from the JSON array
-            let  randomIndex = Math.floor(random[i] * data.length);
-            // Check if the index has been used already
-            while (usedIndices.has(randomIndex)) {
-                // Redraw the random number if the index has been used
-                randomIndex = Math.floor(random[j] * data.length);
-                j++;
-            }
-            usedIndices.add(randomIndex);
-            item.textContent = data[randomIndex];
-            container.appendChild(item);
+}
 
-            if(colors[i] === 'Red') redCount++;
-            if(colors[i] === 'Blue') blueCount++;
-            redCountElement.textContent = redCount; // Update the red count display
-            blueCountElement.textContent = blueCount; // Update the blue count display
-        }
+let type = chechPack()[0];
+if (type === 1) {
+    window.addEventListener('DOMContentLoaded', (event) => {
 
-        // let redButtonClicked = false;
-        // let blueButtonClicked = false;
+        const giturl = chechPack()[1];
+        fetch(giturl)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('grid-container');
+
+
+            const random = getRandomSequenceWithSeed(hash[1], data.length);
+
+            let redCount = 0;
+            let blueCount = 0;
+            let j = 25;
+
+            for(let i = 0; i < 25; i++){
+                const item = document.createElement('div');
+                item.className = 'grid-item';
+                item.dataset.color = colors[i];
+                // const random = Math.random();
+                // console.log(random);
         
-        // const redButton = document.querySelector('.button-red');
-        // redButton.addEventListener('click', () => {
-        //     redButtonClicked = true;
-        //     blueButtonClicked = false; // reset the other flags
-        // });
-
-        // const blueButton = document.querySelector('.button-blue');
-        // blueButton.addEventListener('click', () => {
-        //     blueButtonClicked = true;
-        //     redButtonClicked = false; // reset the other flags
-        // });
-
-        container.addEventListener('click', event => {
-            if(event.target.classList.contains('grid-item')) {
-                // Create and play the audio
-                if (event.target.dataset.clicked) {
-                    // If the item has been clicked already, do nothing and return
-                    return;
+                // Generate a random index to pick an item from the JSON array
+                let  randomIndex = Math.floor(random[i] * data.length);
+                // Check if the index has been used already
+                while (usedIndices.has(randomIndex)) {
+                    // Redraw the random number if the index has been used
+                    randomIndex = Math.floor(random[j] * data.length);
+                    j++;
                 }
-                        // Set the clicked attribute to true
-                event.target.dataset.clicked = true;
-                new Audio('sound/blip-131856.mp3').play();
-                const color = event.target.dataset.color;
+                usedIndices.add(randomIndex);
+                item.textContent = data[randomIndex];
+                container.appendChild(item);
 
-
-                switch(color) {
-                    case 'Red':
-                        event.target.style.backgroundColor = '#be1200';
-                        event.target.style.color = '#f5f5f5';
-                        redCount--;
-                        redCountElement.textContent = redCount; // Update the red count display
-                        break;
-                    case 'Blue':
-                        event.target.style.backgroundColor = '#0000be';
-                        event.target.style.color = '#f5f5f5';
-                        blueCount--;
-                        blueCountElement.textContent = blueCount; // Update the blue count display
-                        break;
-                    case 'Bystander':
-                        event.target.style.backgroundColor = '#bdb5b5';
-                        event.target.style.color = 'black';
-                        break;
-                    case 'Black':
-                        event.target.style.backgroundColor = 'black';
-                        event.target.style.color = '#f5f5f5';
-                        gameOverMessageEl.textContent = 'Game over!';
-                        modal.style.display = "block";
-                        new Audio('sound/gameover-153314.mp3').play();
-                        return;
-                }
-            }
-            if(redCount === 0) {
-                gameOverMessageEl.textContent = 'Red wins!';
-                modal.style.display = "block";
+                if(colors[i] === 'Red') redCount++;
+                if(colors[i] === 'Blue') blueCount++;
                 redCountElement.textContent = redCount; // Update the red count display
-                new Audio('sound/gameover-153314.mp3').play();
-            }
-            if(blueCount === 0) {
-                gameOverMessageEl.textContent = 'Blue wins!';
-                modal.style.display = "block";
                 blueCountElement.textContent = blueCount; // Update the blue count display
-                new Audio('sound/gameover-153314.mp3').play();
             }
 
+            // let redButtonClicked = false;
+            // let blueButtonClicked = false;
+            
+            // const redButton = document.querySelector('.button-red');
+            // redButton.addEventListener('click', () => {
+            //     redButtonClicked = true;
+            //     blueButtonClicked = false; // reset the other flags
+            // });
+
+            // const blueButton = document.querySelector('.button-blue');
+            // blueButton.addEventListener('click', () => {
+            //     blueButtonClicked = true;
+            //     redButtonClicked = false; // reset the other flags
+            // });
+
+            container.addEventListener('click', event => {
+                if(event.target.classList.contains('grid-item')) {
+                    // Create and play the audio
+                    if (event.target.dataset.clicked) {
+                        // If the item has been clicked already, do nothing and return
+                        return;
+                    }
+                            // Set the clicked attribute to true
+                    event.target.dataset.clicked = true;
+                    new Audio('sound/blip-131856.mp3').play();
+                    const color = event.target.dataset.color;
+
+
+                    switch(color) {
+                        case 'Red':
+                            event.target.style.backgroundColor = '#be1200';
+                            event.target.style.color = '#f5f5f5';
+                            redCount--;
+                            redCountElement.textContent = redCount; // Update the red count display
+                            break;
+                        case 'Blue':
+                            event.target.style.backgroundColor = '#0000be';
+                            event.target.style.color = '#f5f5f5';
+                            blueCount--;
+                            blueCountElement.textContent = blueCount; // Update the blue count display
+                            break;
+                        case 'Bystander':
+                            event.target.style.backgroundColor = '#bdb5b5';
+                            event.target.style.color = 'black';
+                            break;
+                        case 'Black':
+                            event.target.style.backgroundColor = 'black';
+                            event.target.style.color = '#f5f5f5';
+                            gameOverMessageEl.textContent = 'Game over!';
+                            modal.style.display = "block";
+                            new Audio('sound/gameover-153314.mp3').play();
+                            return;
+                    }
+                }
+                if(redCount === 0) {
+                    gameOverMessageEl.textContent = 'Red wins!';
+                    modal.style.display = "block";
+                    redCountElement.textContent = redCount; // Update the red count display
+                    new Audio('sound/gameover-153314.mp3').play();
+                }
+                if(blueCount === 0) {
+                    gameOverMessageEl.textContent = 'Blue wins!';
+                    modal.style.display = "block";
+                    blueCountElement.textContent = blueCount; // Update the blue count display
+                    new Audio('sound/gameover-153314.mp3').play();
+                }
+
+            });
+        });
+    
+    });
+}else if (type === 2) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+
+        const giturl = chechPack()[1];
+        fetch(giturl)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('grid-container');
+            const random = getRandomSequenceWithSeed(hash[1], data.length);
+    
+            let redCount = 0;
+            let blueCount = 0;
+            let j = 25;
+    
+            for(let i = 0; i < 25; i++){
+                const item = document.createElement('div');
+                item.className = 'grid-item';
+                item.dataset.color = colors[i];
+            
+                let randomIndex = Math.floor(random[i] * data.length);
+                while (usedIndices.has(randomIndex)) {
+                    randomIndex = Math.floor(random[j] * data.length);
+                    j++;
+                }
+                usedIndices.add(randomIndex);
+                
+                // Create an img element and append it to the item
+                const img = document.createElement('img');
+                img.src = data[randomIndex];
+                img.className = 'grid-img';
+                item.appendChild(img);
+            
+                container.appendChild(item);
+                console.log(data[randomIndex]);
+    
+                if(colors[i] === 'Red') redCount++;
+                if(colors[i] === 'Blue') blueCount++;
+                redCountElement.textContent = redCount; // Update the red count display
+                blueCountElement.textContent = blueCount; // Update the blue count display
+            }
+    
+            container.addEventListener('click', event => {
+                if(event.target.classList.contains('grid-item')) {
+                    if (event.target.dataset.clicked) {
+                        return;
+                    }
+                    event.target.dataset.clicked = true;
+                    new Audio('sound/blip-131856.mp3').play();
+                    const color = event.target.dataset.color;
+            
+                    // Get the img element inside the item
+                    const img = event.target.querySelector('.grid-img');
+            
+                    switch(color) {
+                        case 'Red':
+                            img.src = 'img/red.png';
+                            redCount--;
+                            event.target.style.backgroundColor = '#be1200';
+                            redCountElement.textContent = redCount; 
+                            break;
+                        case 'Blue':
+                            img.src = 'img/blue.png';
+                            blueCount--;
+                            event.target.style.backgroundColor = '#0000be';
+                            blueCountElement.textContent = blueCount;
+                            break;
+                        case 'Bystander':
+                            img.src = 'img/bystander.png';
+                            event.target.style.backgroundColor = '#bdb5b5';
+                            break;
+                        case 'Black':
+                            img.src = 'img/black.png';
+                            gameOverMessageEl.textContent = 'Game over!';
+                            event.target.style.backgroundColor = '#bdb5b5';
+                            modal.style.display = "block";
+                            new Audio('sound/gameover-153314.mp3').play();
+                            return;
+                    }
+    
+                    if(color === 'Black') {
+                        modal.style.display = "block";
+                        gameOverMessageEl.textContent = "Game Over, " + getOppositeTeam(hash[2]) + " Team Wins!";
+                        return;
+                    }
+    
+                    if((redCount === 0 && hash[2] === 'Red') || (blueCount === 0 && hash[2] === 'Blue')) {
+                        modal.style.display = "block";
+                        gameOverMessageEl.textContent = "Game Over, " + hash[2] + " Team Wins!";
+                        return;
+                    }
+    
+                    if((redCount === 0 && hash[2] === 'Blue') || (blueCount === 0 && hash[2] === 'Red')) {
+                        modal.style.display = "block";
+                        gameOverMessageEl.textContent = "Game Over, " + getOppositeTeam(hash[2]) + " Team Wins!";
+                        return;
+                    }
+                }
+            });
         });
     });
-   
-});
+}
 
 
 
-const chechPack = () => {
-        // Get the pack name from localStorage
-        var fragment = window.location.hash;
-        console.log(fragment);
-        fragment = fragment.split('-')[0];
-        if(fragment === '#1'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/defult.json'}
-        else if(fragment === '#2'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greek.json'}
-        else if(fragment === '#3'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/marvel.json'}
-        else if(fragment === '#4'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/starwars.json'}
-        else if(fragment === '#5'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greekhistory.json'}
-        else if(fragment === '#6'){
-        return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/disney.json'}
-        
-    }
 
 function getRandomSequenceWithSeed(seed, length) {
     const a = 1664525;
--- js/solution.js
+++ js/solution.js
@@ -11,98 +11,166 @@ for (let i = colors.length - 1; i > 0; i--) {
 
 let usedIndices = new Set();
 
-window.addEventListener('DOMContentLoaded', (event) => {
-
-    const giturl = chechPack();
-    fetch(giturl)
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById('grid-container');
-
-
-        const random = getRandomSequenceWithSeed(hash[1], data.length);
-
-        let j = 25;
-
-        for(let i = 0; i < 25; i++){
-            const item = document.createElement('div');
-            item.className = 'grid-item';
-            item.dataset.color = colors[i];
-            // const random = Math.random();
-            // console.log(random);
-    
-            // Generate a random index to pick an item from the JSON array
-            let randomIndex = Math.floor(random[i] * data.length);
-
-            while (usedIndices.has(randomIndex)) {
-                // Redraw the random number if the index has been used
-                randomIndex = Math.floor(random[j] * data.length);
-                j++;
-            }
-            usedIndices.add(randomIndex);
-            item.textContent = data[randomIndex];
-
-            const color = item.dataset.color;
-            switch(color) {
-                case 'Red':
-                    item.style.backgroundColor = '#be1200';
-                    item.style.color = '#f5f5f5';
-                    break;
-                case 'Blue':
-                    item.style.backgroundColor = '#0000be';
-                    item.style.color = '#f5f5f5';
-                    break;
-                case 'Bystander':
-                    item.style.backgroundColor = '#bdb5b5';
-                    item.style.color = 'black';
-                    break;
-                case 'Black':
-                    item.style.backgroundColor = 'black';
-                    item.style.color = '#f5f5f5';
-                    break;
-            }
-
-            container.appendChild(item);
-        }
-
-        // let redButtonClicked = false;
-        // let blueButtonClicked = false;
-        
-        // const redButton = document.querySelector('.button-red');
-        // redButton.addEventListener('click', () => {
-        //     redButtonClicked = true;
-        //     blueButtonClicked = false; // reset the other flags
-        // });
-
-        // const blueButton = document.querySelector('.button-blue');
-        // blueButton.addEventListener('click', () => {
-        //     blueButtonClicked = true;
-        //     redButtonClicked = false; // reset the other flags
-        // });
-
-    });
-   
-});
-
-
-const chechPack = () => {
+let chechPack = () => {
     // Get the pack name from localStorage
     var fragment = window.location.hash;
     console.log(fragment);
     fragment = fragment.split('-')[0];
     if(fragment === '#1'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/defult.json'}
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/defult.json']}
     else if(fragment === '#2'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greek.json'}
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greek.json']}
     else if(fragment === '#3'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/marvel.json'}
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/marvel.json']}
     else if(fragment === '#4'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/starwars.json'}
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/starwars.json']}
     else if(fragment === '#5'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greekhistory.json'}
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/greekhistory.json']}
     else if(fragment === '#6'){
-    return 'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/disney.json'}
-    }
+    return [1,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/disney.json']}
+    else if(fragment === '#i6')
+    return [2,'https://raw.githubusercontent.com/MikeTsak/HTML-Codenames/main/packs/idisney.json']
+    
+}
+
+let type = chechPack()[0];
+
+if(type === 1){
+    window.addEventListener('DOMContentLoaded', (event) => {
+
+        const giturl = chechPack()[1];
+        fetch(giturl)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('grid-container');
+
+
+            const random = getRandomSequenceWithSeed(hash[1], data.length);
+
+            let j = 25;
+
+            for(let i = 0; i < 25; i++){
+                const item = document.createElement('div');
+                item.className = 'grid-item';
+                item.dataset.color = colors[i];
+                // const random = Math.random();
+                // console.log(random);
+        
+                // Generate a random index to pick an item from the JSON array
+                let randomIndex = Math.floor(random[i] * data.length);
+
+                while (usedIndices.has(randomIndex)) {
+                    // Redraw the random number if the index has been used
+                    randomIndex = Math.floor(random[j] * data.length);
+                    j++;
+                }
+                usedIndices.add(randomIndex);
+                item.textContent = data[randomIndex];
+
+                const color = item.dataset.color;
+                switch(color) {
+                    case 'Red':
+                        item.style.backgroundColor = '#be1200';
+                        item.style.color = '#f5f5f5';
+                        break;
+                    case 'Blue':
+                        item.style.backgroundColor = '#0000be';
+                        item.style.color = '#f5f5f5';
+                        break;
+                    case 'Bystander':
+                        item.style.backgroundColor = '#bdb5b5';
+                        item.style.color = 'black';
+                        break;
+                    case 'Black':
+                        item.style.backgroundColor = 'black';
+                        item.style.color = '#f5f5f5';
+                        break;
+                }
+
+                container.appendChild(item);
+            }
+
+        });
+    
+    });
+}else if (type === 2) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+
+        const giturl = chechPack()[1];
+        fetch(giturl)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById('grid-container');
+            const random = getRandomSequenceWithSeed(hash[1], data.length);
+            let usedIndices = new Set();
+            let j = 25;
+    
+            for(let i = 0; i < 25; i++){
+                const item = document.createElement('div');
+                item.className = 'grid-item';
+                item.dataset.color = colors[i];
+    
+                let randomIndex = Math.floor(random[i] * data.length);
+                while (usedIndices.has(randomIndex)) {
+                    randomIndex = Math.floor(random[j] * data.length);
+                    j++;
+                }
+                usedIndices.add(randomIndex);
+                
+                // Create an img element and append it to the item
+                const img = document.createElement('img');
+                img.src = data[randomIndex];
+                img.className = 'grid-img';
+                item.appendChild(img);
+            
+                container.appendChild(item);
+                console.log(data[randomIndex]);
+            }
+    
+            container.addEventListener('click', event => {
+                if(event.target.classList.contains('grid-item')) {
+                    if (event.target.dataset.clicked) {
+                        return;
+                    }
+                    event.target.dataset.clicked = true;
+                    new Audio('sound/blip-131856.mp3').play();
+                    const color = item.dataset.color;
+    
+                    // Get the img element inside the item
+    
+                    switch(color) {
+                        case 'Red':
+                            img.src = 'img/red.png';
+                            redCount--;
+                            event.target.style.backgroundColor = '#be1200';
+                            redCountElement.textContent = redCount; 
+                            break;
+                        case 'Blue':
+                            img.src = 'img/blue.png';
+                            blueCount--;
+                            event.target.style.backgroundColor = '#0000be';
+                            blueCountElement.textContent = blueCount;
+                            break;
+                        case 'Bystander':
+                            img.src = 'img/bystander.png';
+                            event.target.style.backgroundColor = '#bdb5b5';
+                            break;
+                        case 'Black':
+                            img.src = 'img/black.png';
+                            gameOverMessageEl.textContent = 'Game over!';
+                            event.target.style.backgroundColor = '#bdb5b5';
+                            modal.style.display = "block";
+                            new Audio('sound/gameover-153314.mp3').play();
+                            return;
+                    }
+                    container.appendChild(item);
+                }
+            });
+        });
+    
+    });
+    
+}
 
 function getRandomSequenceWithSeed(seed, length) {
     const a = 1664525;
